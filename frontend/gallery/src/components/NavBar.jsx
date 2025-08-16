96224caa1fa56d8cdb7956a2e738c234fad65e73
EgorFray <fatamorganaa933@gmail.com>
2025-08-16 19:59:31 +0300

    Frontend: add condition to show SearchBar only on the main page

--- frontend/gallery/src/components/NavBar.jsx
+++ frontend/gallery/src/components/NavBar.jsx
@@ -2,12 +2,17 @@ import styles from "./NavBar.module.css";
 import Search from "../components/Search";
 import Logo from "./Logo";
 import Empty from "./Empty";
+import { useLocation } from "react-router-dom";
+import SearchPlaceholder from "./SearchPlaceholder";
 
 function NavBar() {
+	const location = useLocation();
+	const isMainPage = location.pathname === "/";
 	return (
 		<nav className={styles.navbar}>
 			<Logo />
-			<Search />
+			{/* We neeed this to correctly show borders in the NavBar while <Search /> is hidden */}
+			{isMainPage ? <Search /> : <SearchPlaceholder />}
 			<Empty />
 		</nav>
 	);
---
+++ frontend/gallery/src/components/SearchPlaceholder.jsx
@@ -0,0 +1,7 @@
+import styles from "./SearchPlaceholder.module.css";
+
+function SearchPlaceholder() {
+	return <div className={styles.searchPlaceholder}></div>;
+}
+
+export default SearchPlaceholder;
