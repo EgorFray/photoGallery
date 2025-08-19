c355285b9d4a3ba389db39468edd8a122674aa95
EgorFray <fatamorganaa933@gmail.com>
2025-08-19 20:52:59 +0300

    Frontend: more styling of login page

--- frontend/gallery/src/components/LoginForm.jsx
+++ frontend/gallery/src/components/LoginForm.jsx
@@ -3,19 +3,21 @@ import styles from "./LoginForm.module.css";
 
 function LoginForm() {
 	return (
-		<form className={styles.form}>
-			<div className={styles.row}>
-				<label htmlFor="email">Email</label>
-				<input type="email" id="email" />
-			</div>
+		<div className={styles.formContainer}>
+			<form className={styles.form}>
+				<div className={styles.row}>
+					<label htmlFor="email">Email</label>
+					<input type="email" id="email" />
+				</div>
 
-			<div className={styles.row}>
-				<label htmlFor="password">Password</label>
-				<input type="password" id="password" />
-			</div>
+				<div className={styles.row}>
+					<label htmlFor="password">Password</label>
+					<input type="password" id="password" />
+				</div>
 
-			<Button>Login</Button>
-		</form>
+				<Button className={styles.btnForm}>Login</Button>
+			</form>
+		</div>
 	);
 }
 
