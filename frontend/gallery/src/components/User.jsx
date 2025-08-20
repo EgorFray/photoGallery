88cd8974d29ec8992428c1f0a602b8dcbae66c7f
EgorFray <fatamorganaa933@gmail.com>
2025-08-20 13:08:43 +0300

    Frontend: Fake login implementation - login context

---
+++ frontend/gallery/src/components/User.jsx
@@ -0,0 +1,12 @@
+const FAKE_USER = {
+	name: "Jack",
+	email: "jackexample@gmail.com",
+	password: "qwerty",
+	avatar: "https://i.pravatar.cc/100?u=zz",
+};
+
+function User() {
+	return <div></div>;
+}
+
+export default User;
