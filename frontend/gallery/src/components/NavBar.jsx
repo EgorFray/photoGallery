645d757aebde43f5ffadfe5c6095223b27868826
EgorFray <fatamorganaa933@gmail.com>
2025-08-21 22:31:26 +0300

    Frontend: add User styles and add user icon to the navBar

--- frontend/gallery/src/components/NavBar.jsx
+++ frontend/gallery/src/components/NavBar.jsx
@@ -2,18 +2,22 @@ import styles from "./NavBar.module.css";
 import Search from "../components/Search";
 import Logo from "./Logo";
 import Empty from "./Empty";
-import { useLocation } from "react-router-dom";
+import { useActionData, useLocation } from "react-router-dom";
 import SearchPlaceholder from "./SearchPlaceholder";
+import { useAuth } from "../context/FakeAuthContext";
+import User from "./User";
 
 function NavBar() {
 	const location = useLocation();
 	const isMainPage = location.pathname === "/app";
+	const { isAuthenticated } = useAuth();
+
 	return (
 		<nav className={styles.navbar}>
 			<Logo />
 			{/* We neeed this to correctly show borders in the NavBar while <Search /> is hidden */}
 			{isMainPage ? <Search /> : <SearchPlaceholder />}
-			<Empty />
+			{isAuthenticated ? <User /> : <Empty />}
 		</nav>
 	);
 }
--- frontend/gallery/src/components/User.jsx
+++ frontend/gallery/src/components/User.jsx
@@ -1,12 +1,22 @@
-const FAKE_USER = {
-	name: "Jack",
-	email: "jackexample@gmail.com",
-	password: "qwerty",
-	avatar: "https://i.pravatar.cc/100?u=zz",
-};
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/FakeAuthContext";
+import styles from "./User.module.css";
 
 function User() {
-	return <div></div>;
+	const { user, logout } = useAuth();
+	const navigate = useNavigate();
+
+	function handleClick() {
+		logout();
+		navigate("/login");
+	}
+
+	return (
+		<div className={styles.user}>
+			<img src={user.avatar} alt={user.name} />
+			<button onClick={handleClick}>Logout</button>
+		</div>
+	);
 }
 
 export default User;
