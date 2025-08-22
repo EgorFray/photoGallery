7f3eaafd0b496bef2e88c9f36b8a209309c05309
EgorFray <fatamorganaa933@gmail.com>
2025-08-22 23:52:27 +0300

    Frontend: add Protected Route; IMPORTANT: fix routes in the future - add one universal path and childrens

--- frontend/gallery/src/App.jsx
+++ frontend/gallery/src/App.jsx
@@ -7,6 +7,7 @@ import PageNotFound from "./pages/PageNotFound";
 import Homepage from "./pages/Homepage";
 import Login from "./pages/Login";
 import { AuthProvider } from "./context/FakeAuthContext";
+import ProtectedRoute from "./pages/ProtectedRoute";
 
 function App() {
 	return (
@@ -16,9 +17,30 @@ function App() {
 					<Routes>
 						<Route index element={<Homepage />} />
 						<Route path="/login" element={<Login />} />
-						<Route path="/app" element={<MainPage />} />
-						<Route path="/create" element={<CreatePost />} />
-						<Route path="/app/posts/:id" element={<PostDetail />} />
+						<Route
+							path="/app"
+							element={
+								<ProtectedRoute>
+									<MainPage />
+								</ProtectedRoute>
+							}
+						/>
+						<Route
+							path="/create"
+							element={
+								<ProtectedRoute>
+									<CreatePost />
+								</ProtectedRoute>
+							}
+						/>
+						<Route
+							path="/app/posts/:id"
+							element={
+								<ProtectedRoute>
+									<PostDetail />
+								</ProtectedRoute>
+							}
+						/>
 						<Route path="*" element={<PageNotFound />} />
 					</Routes>
 				</PostsProvider>
