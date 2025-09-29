f6a5022a4c7f06dd6aaee948d48b10cc069d0403
EgorFray <fatamorganaa933@gmail.com>
2025-09-29 21:35:18 +0300

    Backend: created first test of dbCallCreatePost

--- backend/internal/repository/posts/posts_repo_test.go
+++ backend/internal/repository/posts/posts_repo_test.go
@@ -4,6 +4,8 @@ import (
 	"database/sql"
 	"log"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 	_ "modernc.org/sqlite"
 )
 
@@ -11,7 +13,7 @@ import (
 func setupTestDB(t *testing.T) *sql.DB {
 	db, err := sql.Open("sqlite", ":memory:")
 	if err != nil {
-		log.Fatalf("failed to open sqlite", err)
+		log.Fatalf("failed to open sqlite: %v", err)
 	} 
 
 	_, err = db.Exec(`
@@ -25,12 +27,24 @@ func setupTestDB(t *testing.T) *sql.DB {
 	`)
 
 	if err != nil {
-		log.Fatalf("failed to creat table posts: %v", err)
+		log.Fatalf("failed to create table posts: %v", err)
 	}
 
 	return db
 
 }
 
-func TestDbCallGetPostById(t *testing.T) {
+func TestDbCallCreatePost(t *testing.T) {
+	db := setupTestDB(t)
+	repo := NewPostRepository(db)
+
+	insertedID, err := repo.DbCallCreatePost("/images/test-img.jpg", "test", "1")
+	assert.NoError(t, err)
+	assert.NotZero(t, insertedID)
+
+	row := db.QueryRow("SELECT description FROM posts WHERE id = ?", insertedID)
+	var desc string
+	err = row.Scan(&desc)
+	assert.NoError(t, err)
+	assert.Equal(t, "test", desc)
 }
