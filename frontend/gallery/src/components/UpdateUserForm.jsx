b1c7e73ba6ea262feabb1dd8f7c89a635270a4aa
EgorFray <fatamorganaa933@gmail.com>
2025-10-23 16:33:53 +0300

    Frontend: add states and effect for span

--- frontend/gallery/src/components/UpdateUserForm.jsx
+++ frontend/gallery/src/components/UpdateUserForm.jsx
@@ -1,39 +1,66 @@
+import { motion } from "motion/react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/FakeAuthContext";
 import Button from "./Button";
 import styles from "./UpdateUserForm.module.css";
 
 function UpdateUserForm() {
+	const { user } = useAuth();
+
+	const [newName, setNewName] = useState(user.name);
+	const [newPassword, setNewPassword] = useState("");
+
+	const navigate = useNavigate();
+
+	async function handleSubmit(e) {
+		e.preventDefault();
+
+		const formData = new FormData(e.target);
+		await updateUser(formData);
+		navigate("/profile");
+	}
+
 	return (
 		<div className={styles.formContainer}>
 			<p className={styles.updateHeading}>
 				You don't have to update all field at once
 			</p>
-			<span className={styles.updateHeadingMotion}>Update only neccessary</span>
-			<form className={styles.updateForm}>
+			<motion.span
+				className={styles.updateHeadingMotion}
+				initial={{ clipPath: "inset(0 100% 0 0)" }}
+				animate={{ clipPath: "inset(0 0% 0 0)" }}
+				transition={{ duration: 1.2, ease: "easeInOut", delay: 1.5 }}
+			>
+				Update only neccessary
+			</motion.span>
+
+			<form className={styles.updateForm} onSubmit={handleSubmit}>
 				<div className={styles.row}>
-					<label htmlFor="name">Name</label>
+					<label htmlFor="name">New name</label>
 					<input
 						type="text"
 						id="name"
 						name="name"
-						// onChange={(e) => setName(e.target.value)}
-						// value={name}
+						onChange={(e) => setNewName(e.target.value)}
+						value={newName}
 					/>
 				</div>
 
 				<div className={styles.row}>
-					<label htmlFor="password">Password</label>
+					<label htmlFor="password">New password</label>
 					<input
 						type="password"
 						id="password"
 						name="password"
-						// onChange={(e) => setPassword(e.target.value)}
-						// value={password}
+						onChange={(e) => setNewPassword(e.target.value)}
+						value={newPassword}
 					/>
 				</div>
 
 				<div className={styles.row}>
 					<label htmlFor="avatar" className={styles.addAvatar}>
-						Add avatar
+						New avatar
 					</label>
 					<input type="file" name="avatar" id="avatar" />
 				</div>
