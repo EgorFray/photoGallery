8e75df0edd1c4b6bee3beb528d44fd356040bb9a
EgorFray <fatamorganaa933@gmail.com>
2025-10-28 16:07:08 +0200

    Frontend: fix bug while updating user data on the main page

--- frontend/gallery/src/components/User.jsx
+++ frontend/gallery/src/components/User.jsx
@@ -1,18 +1,23 @@
+import { useEffect } from "react";
 import { useState } from "react";
-import { useAuth } from "../context/FakeAuthContext";
+import { useUser } from "../context/UserContext";
 import styles from "./User.module.css";
 import UserDropdown from "./UserDropdown";
 
 function User() {
 	const [isOpenDrop, setIsOpenDrop] = useState(false);
-	const { user } = useAuth();
+	const { curUser, getCurrentUser } = useUser();
+
+	useEffect(function () {
+		getCurrentUser();
+	}, []);
 
 	return (
 		<>
 			<div className={styles.user}>
 				<img
-					src={`${import.meta.env.VITE_BACKEND_URL}${user.avatar}`}
-					alt={user.name}
+					src={`${import.meta.env.VITE_BACKEND_URL}${curUser.avatar}`}
+					alt={curUser.name}
 					onClick={() => setIsOpenDrop(!isOpenDrop)}
 				/>
 			</div>
